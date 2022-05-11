136852ffcd85d0dbde3875a2e4ca6c6ad77ad547
BraydenAnimati0ns <braydenmcisaackennett2013@gmail.com>
2022-05-11 09:16:51 -0400

    Add files via upload

---
+++ cameraAccess.js
@@ -0,0 +1,11 @@
+var front = false;
+var video = document.querySelector('video');
+var constraints = { video: { facingMode: (front? "user" : "environment"), width: 640, height: 480  } };
+navigator.mediaDevices.getUserMedia(constraints)
+.then(function(mediaStream) {
+video.srcObject = mediaStream;
+video.onloadedmetadata = function(e) {
+video.play();
+};
+})
+.catch(function(err) { console.log(err.name + ": " + err.message); })
